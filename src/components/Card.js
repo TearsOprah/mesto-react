5db1e62c9bafab0f5f8b323efd21e69cf2ab574b
TearsOprah <qmeshokp@mail.ru>
2023-03-13 10:33:49 +0200

    feat: отображение собственного лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,16 @@ export default function Card(props) {
 
   const currentUser = React.useContext(CurrentUserContext)
 
+  // Определяем, являемся ли мы владельцем текущей карточки
   const isOwn = props.card.owner._id === currentUser._id;
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = (
+    `element__like ${isLiked && 'element__like_active'}`
+  );
+
 
   function handleCardClick() {
     props.onCardClick(props.card)
@@ -16,7 +25,7 @@ export default function Card(props) {
       <div className="element__info">
         <h2 className="element__title">{props.card.name}</h2>
         <div>
-          <button className="element__like" type="button"></button>
+          <button className={cardLikeButtonClassName} type="button"></button>
           <p className="element__likes">{props.card.likes.length}</p>
         </div>
       </div>
