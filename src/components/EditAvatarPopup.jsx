6044dc725f33f9b4abc04a2dc39124c6ecc8e559
TearsOprah <qmeshokp@mail.ru>
2023-03-13 18:53:24 +0200

    feat: валидация формы в EditAvatarPopup

--- src/components/EditAvatarPopup.jsx
+++ src/components/EditAvatarPopup.jsx
@@ -3,19 +3,35 @@ import React from "react";
 
 export default function EditAvatarPopup(props) {
 
+  const avatarErrorRef = React.useRef();
   const avatarRef = React.useRef();
 
   function handleSubmit(e) {
     e.preventDefault();
 
-    props.onUpdateAvatar({
-      avatar: avatarRef.current.value /* Значение инпута, полученное с помощью рефа */,
-    });
+    try {
+      new URL(avatarRef.current.value);
+      props.onUpdateAvatar({
+        avatar: avatarRef.current.value /* Значение инпута, полученное с помощью рефа */,
+      });
+      // скидываем вписанное значение
+      avatarRef.current.value = ''
+    } catch (err) {
+      avatarErrorRef.current.textContent = 'Введите корректный URL';
+    }
+  }
+
+  function handleClose() {
+    props.onClose()
+    // скидываем сообщение об ошибке
+    avatarErrorRef.current.textContent = ''
+    // скидываем вписанное значение
+    avatarRef.current.value = ''
   }
 
   return (
     <PopupWithForm  isOpen={props.isOpen}
-                    onClose={props.onClose}
+                    onClose={handleClose}
                     name={'avatar'}
                     title={'Обновить аватар'}
                     buttonText={'Сохранить'}
@@ -24,7 +40,7 @@ export default function EditAvatarPopup(props) {
       <div className="popup__field-container">
         <input id="avatarInput" className="popup__field" type="url" name="avatar" maxLength="1000"
                placeholder="Ссылка на картинку" required ref={avatarRef}/>
-        <span id="avatarInput-error" className="popup__error popup__error_visible"></span>
+        <span ref={avatarErrorRef} id="avatarInput-error" className="popup__error popup__error_visible"></span>
       </div>
 
     </PopupWithForm>
