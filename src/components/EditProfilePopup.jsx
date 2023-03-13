af813c8beed92e526bd7549a5a5f8c7ba5dc16c7
TearsOprah <qmeshokp@mail.ru>
2023-03-13 18:37:47 +0200

    feat: валидация формы в EditProfilePopup

--- src/components/EditProfilePopup.jsx
+++ src/components/EditProfilePopup.jsx
@@ -2,6 +2,7 @@ import PopupWithForm from "./PopupWithForm";
 import {useContext, useState} from "react";
 import CurrentUserContext from "../contexts/CurrentUserContext";
 import React from "react";
+import {useRef} from "react";
 
 export default function EditProfilePopup(props) {
 
@@ -9,6 +10,9 @@ export default function EditProfilePopup(props) {
   const currentUser = useContext(CurrentUserContext);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
+
+  const nameErrorRef = useRef();
+  const descriptionErrorRef = useRef();
 
   // После загрузки текущего пользователя из API
   // его данные будут использованы в управляемых компонентах.
@@ -29,16 +33,53 @@ export default function EditProfilePopup(props) {
     // Запрещаем браузеру переходить по адресу формы
     event.preventDefault();
 
+    // переменные с текстом ошибок
+    let nameError = ''
+    let descriptionError = ''
+
+    // проверяем поле имени
+    if (name.length < 2 || name.length > 40) {
+      nameError = ('Имя должно содеражать от 2 до 40 символов')
+    }
+
+    // проверяем поле профессии
+    if (description.length < 2 || description.length > 200) {
+      descriptionError = ('профессия должна содержать от 2 до 200 символов')
+    }
+
+    // если есть ошибки - выводим и делаем span видимыми
+    if (nameError || descriptionError) {
+      nameErrorRef.current.textContent = nameError;
+      descriptionErrorRef.current.textContent = descriptionError;
+      nameErrorRef.current.classList.toggle('popup__error_visible', nameError !== '');
+      descriptionErrorRef.current.classList.toggle('popup__error_visible', descriptionError !== '');
+      return
+    }
+
     // Передаём значения управляемых компонентов во внешний обработчик
     props.onUpdateUser({
       name,
       about: description,
     });
+
+    // обнуляем ошибки после отправки формы
+    nameErrorRef.current.textContent = '';
+    descriptionErrorRef.current.textContent = '';
+  }
+
+  function handleClose() {
+    props.onClose()
+    // обнуляем ошибки после закрытия
+    nameErrorRef.current.textContent = '';
+    descriptionErrorRef.current.textContent = '';
+    // чтобы после закрытия попапа с ошибкой, в поле вернулось старое имя
+    setName(currentUser.name);
+    setDescription(currentUser.about)
   }
 
   return (
     <PopupWithForm isOpen={props.isOpen}
-                   onClose={props.onClose}
+                   onClose={handleClose}
                    name={'edit'}
                    title={'Редактировать профиль'}
                    buttonText={'Сохранить'}
@@ -47,14 +88,22 @@ export default function EditProfilePopup(props) {
 
       <>
         <div className="popup__field-container">
-          <input onChange={handleChangeName} value={name || ''} id="nameInput" className="popup__field" type="text" name="name" minLength="2" maxLength="40"
-                 placeholder="Введите имя" required />
-          <span id="nameInput-error" className="popup__error"></span>
+          <input onChange={handleChangeName}
+                 value={name || ''}
+                 id="nameInput"
+                 className="popup__field"
+                 type="text"
+                 name="name"
+                 minLength="2"
+                 maxLength="40"
+                 placeholder="Введите имя"
+                 required />
+          <span ref={nameErrorRef} id="nameInput-error" className="popup__error"></span>
         </div>
         <div className="popup__field-container">
           <input onChange={handleChangeDescription} value={description || ''} id="jobInput" className="popup__field" type="text" name="job" minLength="2" maxLength="200"
                  placeholder="Введите профессию" required />
-          <span id="jobInput-error" className="popup__error"></span>
+          <span ref={descriptionErrorRef} id="jobInput-error" className="popup__error"></span>
         </div>
       </>
 
